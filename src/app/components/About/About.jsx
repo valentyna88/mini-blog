d35ce49fe2079a3ff8069913fe0283ad758a0cae
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-07-23 10:39:39 +0300

    style(about): add styles for About page

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -1,15 +1,19 @@
+import styles from "./About.module.css";
+
 export default function About({ dict }) {
   return (
-    <div>
-      <h1>{dict.about.title}</h1>
-      <p>{dict.about.desc}</p>
-      <h3>{dict.about.team}</h3>
-      <ul>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{dict.about.title}</h1>
+      <p className={styles.desc}>{dict.about.desc}</p>
+      <h3 className={styles.subtitle}>{dict.about.team}</h3>
+      <ul className={styles.list}>
         {dict.about.names.map((name, idx) => (
-          <li key={idx}>{name}</li>
+          <li key={idx} className={styles.item}>
+            {name}
+          </li>
         ))}
       </ul>
-      <p>{dict.about.note}</p>
+      <p className={styles.note}>{dict.about.note}</p>
     </div>
   );
 }
