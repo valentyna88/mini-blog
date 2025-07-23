1e2ded66edf41fa799f099887669dafdc32aeb97
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-07-23 11:27:47 +0300

    style(footer): add Footer styles

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,14 +1,10 @@
+import styles from "./Footer.module.css";
+
 export default function Footer({ dict }) {
   return (
-    <footer
-      style={{
-        padding: "16px",
-        borderTop: "1px solid #eee",
-        textAlign: "center",
-      }}
-    >
-      &copy; {new Date().getFullYear()} Valentyna Taranchuk{" "}
-      {dict.footer.copyright}
+    <footer className={styles.footer}>
+      &copy; {new Date().getFullYear()} Valentyna Taranchuk —{" "}
+      {dict.footer?.copyright}
     </footer>
   );
 }
